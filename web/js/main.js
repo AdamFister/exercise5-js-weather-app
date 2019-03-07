2649676872b25903f856312f393895c7253eafbb
Adam Fister <arfister@gmail.com>
2019-03-07 17:01:22 -0500

    build app

---
+++ web/js/main.js
@@ -0,0 +1,28 @@
+
+function myFunction() {
+    let zipCode = document.getElementById("zipCode").value;
+    fetch('http://api.openweathermap.org/data/2.5/forecast?zip=' + zipCode + '&APPID=53f03e4e31f2a70d7b4aa1cdf081e981')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    let tempKelvin = JSON.stringify(myJson.list[0].main.temp);
+    let city = JSON.stringify(myJson.city.name);
+    let condition = JSON.stringify(myJson.list[0].weather[0].description);
+
+    let tempNumKelvin = parseFloat(tempKelvin);
+    let tempFahrenheit = (tempNumKelvin - 273.15) * 9/5 + 32;
+    let tempCelsius = tempNumKelvin - 273 ;
+
+    document.getElementById("TempKelvinHere").innerHTML = Math.round(tempNumKelvin) + " K";
+    document.getElementById("TempFahrenheitHere").innerHTML = Math.round(tempFahrenheit) + "°F";
+    document.getElementById("TempCelsiusHere").innerHTML = Math.round(tempCelsius) + "°C";
+    document.getElementById("CityHere").innerHTML = city;
+    document.getElementById("ConditionHere").innerHTML = condition;
+  });  
+}
+
+
+
+
+
